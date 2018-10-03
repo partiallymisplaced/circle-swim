29757c1c272397ebf460631c637429bbd80d82de
Nevena Stoeva <nevena.stoeva@gmail.com>
2018-10-02 18:13:25 -0700

    Adds data fetched from Data Seattle Gov API

--- src/App.js
+++ src/App.js
@@ -7,11 +7,40 @@ import Map from './Map';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      poolList: [],
+    };
+  }
+
+  getPoolData() {
+    fetch('https://data.seattle.gov/resource/ppq2-qxkx', {
+      method: "GET",
+      mode: 'cors',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: {
+        "app_token": "XYEEYf573j7hRlczkT6WSAZKK",
+      },
+    })
+    .then(response => response.json())
+    .then(data => this.setState({
+      poolList: data
+    }));
+  }
+
+  componentDidMount() {
+    this.getPoolData();
+  }
+
   render() {
     return (
     <main>
-      <PoolList />
-      <Map />
+      <PoolList poolList = {this.state.poolList}/>
+      <Map poolList = {this.state.poolList}/>
     </main>
     );
   }
