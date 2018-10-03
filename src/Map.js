4468227967f5dd5683845c962069989295db63b3
Nevena Stoeva <nevena.stoeva@gmail.com>
2018-10-02 18:14:26 -0700

    Adds initial styling of PoolList, Map components

--- src/Map.js
+++ src/Map.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Map extends Component {
   render() {
     return (
-      <div>
+      <div id="map" className="pools-map">
         <p>There be a map.</p>
       </div>
     )
