8666fc2252df5d7f0a9ae054fde1dbe153d8da7c
Nevena Stoeva <nevena.stoeva@gmail.com>
2018-10-03 11:57:16 -0700

    Adds rendering of fetched data to UI

--- src/Pool.js
+++ src/Pool.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 class Pool extends Component {
   render() {
     return (
-      <div>
-        <p>There be pools.</p>
+      <div className="pool-container">
+        <p className="pool-name">{this.props.pool.name}</p>
+        <p className="pool-address">{this.props.pool.address}</p>
+        <p className="pool-type">{this.props.pool.indoor_out}</p>
+        <hr />
       </div>
     )
   }
--- src/PoolList.js
+++ src/PoolList.js
@@ -3,11 +3,30 @@ import Pool from './Pool';
 
 class PoolList extends Component {
   render() {
+    console.log('props', this.props)
+
     return (
       <aside>
       <h1>Circle Swim</h1>
+      <select>
+        <option>Any</option>
+        <option>Indoor</option>
+        <option>Outdoor</option>
+      </select>
       <div>
-        <Pool />
+        <ol>
+          {
+            this.props.poolList
+            .map(pool => (
+              <li key={pool.pmaid}>
+                <Pool
+                  pool={pool}
+                />
+              </li>
+
+            ))
+          }
+        </ol>
       </div>
       </aside>
     )
